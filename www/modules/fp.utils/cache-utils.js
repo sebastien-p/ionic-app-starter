c88ec0cde52a03532cc3ea8b3e970c502bf43b11
Sebastien PITTION <sp@fing.pro>
2015-11-30 15:11:50 +0100

    Add caching utilities

--- www/modules/fp.utils/cache-utils.js
+++ www/modules/fp.utils/cache-utils.js
@@ -7,9 +7,10 @@
   /**
    * Some caching utilities.
    * @constructor CacheUtils
+   * @param {Object} $q - The Angular $q service.
    * @param {Object} cacheFactory - The Angular Cache CacheFactory service.
    */
-  function CacheUtils(cacheFactory) {
+  function CacheUtils($q, cacheFactory) {
     var service = this;
 
     /**
@@ -32,8 +33,35 @@
           deleteOnExpire: 'passive'
       }, options));
     };
+
+    /**
+     * Get or create a cache associated to a given module.
+     * @method getModuleCache
+     * @param {Object} module - An angular module object.
+     * @param {Object|Number} [options] - A CacheFactory options object or
+     * the cache validity duration in minutes.
+     * @param {String} [options.storageMode='localStorage']
+     * @param {String} [options.deleteOnExpire='passive']
+     * @return {Object}
+     */
+    service.getModuleCache = function (module, options) {
+      return service.getCache(module.name, options);
+    };
+
+    /**
+     * Resolve a cached value if any.
+     * @private
+     * @function resolveCachedValue
+     * @param {Object} cache - The cache object potentially storing the value.
+     * @param {String} key - The key associated to the cached value.
+     * @return {Promise|undefined} - Undefined if not cached.
+     */
+    service.resolveCachedValue = function (cache, key) {
+      var value = cache && cache.get(key);
+      if (value) { return $q.when(value); }
+    };
   }
 
-  module.service('cacheUtils', ['CacheFactory', CacheUtils]);
+  module.service('cacheUtils', ['$q', 'CacheFactory', CacheUtils]);
 
 }(angular.module('fp.utils')));
