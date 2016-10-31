77e267075fd11c63ef5f0dc0aac7b651b2f1642b
Sebastien PITTION <sp@fing.pro>
2016-10-31 13:03:33 +0100

    Add and fix unit tests

--- test/unit/app/app.spec.js
+++ test/unit/app/app.spec.js
@@ -2,59 +2,40 @@
 
 describe('[app]', function () {
   var $rootScope;
-  var stateFolderProvider;
+  var cordovaUtils;
+  var APP_VERSION;
   var APP_NAME;
 
-  beforeEach(module('app', function (
-    _stateFolderProvider_
-  ) {
-    stateFolderProvider = _stateFolderProvider_;
-  }));
+  beforeEach(module('app'));
 
   beforeEach(inject(function (
     _$rootScope_,
+    _cordovaUtils_,
+    _APP_VERSION_,
     _APP_NAME_
   ) {
     $rootScope = _$rootScope_;
+    cordovaUtils = _cordovaUtils_;
+    APP_VERSION = _APP_VERSION_;
     APP_NAME = _APP_NAME_;
   }));
 
-  it('should set the app name on the root scope', function () {
-    expect(_.isString($rootScope.APP_NAME)).toBe(true);
-    expect($rootScope.APP_NAME).toBeTruthy();
-    expect($rootScope.APP_NAME).toBe(APP_NAME);
-  });
-
-  describe('stateFolderProvider', function () {
-    it('should have a FOLDERS property', function () {
-      expect(_.isPlainObject(stateFolderProvider.FOLDERS)).toBe(true);
-      expect(stateFolderProvider.FOLDERS.SMARTPHONE).toBe('smartphone');
-      expect(stateFolderProvider.FOLDERS.TABLET).toBe('tablet');
+  describe('$rootScope', function () {
+    it('should set `IS_CORDOVA` on the root scope', function () {
+      expect(_.isBoolean($rootScope.IS_CORDOVA)).toBe(true);
+      expect($rootScope.IS_CORDOVA).toBe(cordovaUtils.isCordova());
     });
 
-    describe('stateFolderProvider.getFolder', function () {
-      it('should have a getFolder method', function () {
-        expect(_.isFunction(stateFolderProvider.getFolder)).toBe(true);
-      });
-
-      it('should return the smartphone folder by default', function () {
-        expect(stateFolderProvider.getFolder())
-          .toBe(stateFolderProvider.FOLDERS.SMARTPHONE);
-      });
+    it('should set the app version on the root scope', function () {
+      expect(_.isString($rootScope.APP_VERSION)).toBe(true);
+      expect($rootScope.APP_VERSION).toBeTruthy();
+      expect($rootScope.APP_VERSION).toBe(APP_VERSION);
     });
 
-    describe('stateFolderProvider.setFolder', function () {
-      it('should have a setFolder method', function () {
-        expect(_.isFunction(stateFolderProvider.setFolder)).toBe(true);
-      });
-
-      it('should set the given folder', function () {
-        var FOLDERS = stateFolderProvider.FOLDERS;
-        stateFolderProvider.setFolder(FOLDERS.SMARTPHONE);
-        expect(stateFolderProvider.getFolder()).toBe(FOLDERS.SMARTPHONE);
-        stateFolderProvider.setFolder(FOLDERS.TABLET);
-        expect(stateFolderProvider.getFolder()).toBe(FOLDERS.TABLET);
-      });
+    it('should set the app name on the root scope', function () {
+      expect(_.isString($rootScope.APP_NAME)).toBe(true);
+      expect($rootScope.APP_NAME).toBeTruthy();
+      expect($rootScope.APP_NAME).toBe(APP_NAME);
     });
   });
 });
