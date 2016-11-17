50c1f5120793487e5bb63eeb8b9c9a69d53410a0
Sebastien PITTION <sp@fing.pro>
2016-11-17 14:38:56 +0100

    directly return the promise

--- gulp/tasks/build.clean.js
+++ gulp/tasks/build.clean.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var del = require('del');
-var ary = require('lodash').ary;
 
 /**
  * Remove unused directories after a build process.
@@ -9,13 +8,13 @@ var ary = require('lodash').ary;
  * @param {Object} plugins - Gulp plugins loaded by *gulp-load-plugins* and
  *   passed to *gulp-load-tasks*.
  * @param {Object} config - Gulp config object passed to *gulp-load-tasks*.
- * @param {Function} done - Function to call when asynchrounous stuff is done.
+ * @return {Promise}
  */
-function gulpBuildClean(gulp, plugins, config, done) {
+function gulpBuildClean(gulp, plugins, config) {
   var task = config.tasks['build.clean'];
   var opt = { cwd: task.cwd || '', nomount: true, strict: true, mark: true };
 
-  del(task.src, opt).then(ary(done, 0)).catch(done);
+  return del(task.src, opt);
 }
 
 module.exports = [['build.rev.replace'], gulpBuildClean];
--- gulp/tasks/clean.cordova.js
+++ gulp/tasks/clean.cordova.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var del = require('del');
-var ary = require('lodash').ary;
 
 /**
  * Remove Cordova-related directories and files to get a fresh working folder.
@@ -9,13 +8,13 @@ var ary = require('lodash').ary;
  * @param {Object} plugins - Gulp plugins loaded by *gulp-load-plugins* and
  *   passed to *gulp-load-tasks*.
  * @param {Object} config - Gulp config object passed to *gulp-load-tasks*.
- * @param {Function} done - Function to call when asynchrounous stuff is done.
+ * @return {Promise}
  */
-function gulpCleanCordova(gulp, plugins, config, done) {
+function gulpCleanCordova(gulp, plugins, config) {
   var task = config.tasks['clean.cordova'];
   var opt = { cwd: task.cwd || '', nomount: true, strict: true, mark: true };
 
-  del(task.src, opt).then(ary(done, 0)).catch(done);
+  return del(task.src, opt);
 }
 
 module.exports = [gulpCleanCordova];
